f7a2ab452e0ab432818c763c11b81a0a211dfe10
Vipin Sharma <sh.vipin@gmail.com>
2021-02-16 13:48:40 -0800

    sql30: Enable Python2 tests.
    Enable python 2 tests.

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,9 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Running tests in python 3"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Running tests in python 3"
-python3 -m unittest discover -p "*.py" sql30/tests/ -v
+python3 -m unittest discover -p "test_*.py" sql30/tests/ -v
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "Running tests in python 2- DIABLED"
-# python2 -m unittest discover -p "*.py" sql30/tests/ -v
+echo "Running tests in python 2 "
+python2 -m unittest discover -p "test_*.py" sql30/tests/ -v
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
